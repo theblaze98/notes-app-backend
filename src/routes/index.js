d1042ac36fc7eddcb9249eb99282410d063b5b04
Shammael <shammamnd2015@gmail.com>
2023-04-29 16:04:31 -0400

    feat: :sparkles: add note routes to index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './auth/index.js';
 import userRoutes from './user/index.js';
+import noteRoutes from './notes/index.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
+router.use('/note', noteRoutes);
 
 export default router;
