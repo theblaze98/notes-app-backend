bcb46d4665d78a8cd7c4f3ff9a2eec6baf032acb
Shammael <shammamnd2015@gmail.com>
2023-04-29 15:03:22 -0400

    feat: :sparkles: add userRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRoutes from './auth/index.js';
+import userRoutes from './user/index.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/user', userRoutes);
 
 export default router;
