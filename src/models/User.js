c5105d996fefcd668df3fba7c07990bf6283a5e2
Shammael <shammamnd2015@gmail.com>
2023-04-29 09:18:26 -0400

    refactor: :fire: Clean User schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,34 +1,14 @@
-import { Schema, Types, model } from "mongoose";
-import bcryptjs from "bcryptjs";
-import JWT from "jsonwebtoken";
-
-const { hash, compare } = bcryptjs;
+import { Schema, model } from 'mongoose';
 
 const UserSchema = new Schema(
   {
     username: { type: String, required: true, trim: true, unique: true },
     email: { type: String, required: true, trim: true, unique: true },
-    password: { type: String, required: true, select: false },
+    password: { type: String, required: true, select: false }
   },
   {
-    timestamps: true,
+    timestamps: true
   }
 );
 
-UserSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await hash(this.password, 8);
-  }
-});
-
-UserSchema.methods.comparePassword = async function (password) {
-  return await compare(password, this.password);
-};
-
-UserSchema.methods.getToken = function () {
-  return JWT.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
-  });
-};
-
-export default model("user", UserSchema);
+export default model('user', UserSchema);
