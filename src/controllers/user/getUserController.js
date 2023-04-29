7adb4f8bb85bff558d0694030a4eeacf6f2bcdd0
Shammael <shammamnd2015@gmail.com>
2023-04-29 13:51:08 -0400

    fix: :bug: update getUserControlller

--- src/controllers/user/getUserController.js
+++ src/controllers/user/getUserController.js
@@ -1,7 +1,8 @@
 import { getUserService } from '../../services';
 
 const getUserController = async (req, res) => {
-  const userDB = await getUserService(req.user);
+  const { id } = req.query;
+  const userDB = await getUserService({ id });
   res.status(200).json({
     username: userDB.username,
     email: userDB.email
