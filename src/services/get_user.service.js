077fd8e4f6a7a6d12f731b1534be68df586c7a1f
Shammael <shammamnd2015@gmail.com>
2023-04-29 13:45:30 -0400

    fix: :bug: add _id instead of id

--- src/services/get_user.service.js
+++ src/services/get_user.service.js
@@ -4,7 +4,7 @@ const getUserService = async ({ id, email, username }, { operator }) => {
   if (operator === 'OR') {
     return await UserModel.findOne({ $or: [{ email, username }] });
   }
-  return await UserModel.findOne({ id, email, username });
+  return await UserModel.findOne({ _id: id, email, username });
 };
 
 export default getUserService;
