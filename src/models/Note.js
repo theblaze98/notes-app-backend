cb9415dfe0226b208093bddee29e3d649d67c857
Shammael <shammamnd2015@gmail.com>
2023-04-29 09:22:57 -0400

    style: :lipstick: bug fix and styling

--- src/models/Note.js
+++ src/models/Note.js
@@ -1,22 +1,25 @@
 import { Schema, Types, model } from 'mongoose';
 
-const NoteSchema = new Schema({
+const NoteSchema = new Schema(
+  {
     userID: {
-        type: Types.ObjectId,
-        required: true,
-        inmutable: true,
-        res: 'user'
+      type: Types.ObjectId,
+      required: true,
+      inmutable: true,
+      ref: 'user'
     },
     title: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true
     },
     description: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true
     }
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 export default model('note', NoteSchema);
