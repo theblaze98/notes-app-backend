892e3ed31d1b66241dbcefd6eba1c74e43bd15a6
Shammael <shammamnd2015@gmail.com>
2023-04-30 18:07:39 -0400

    style: :truck: rename file and name function of create note

--- src/controllers/notes/create_notes.controller.js
+++
@@ -1,9 +0,0 @@
-import { createNoteService } from '../../services/index.js';
-
-const createNotesController = async (req, res) => {
-  const { title, description } = req.body;
-  await createNoteService({ title, description, userID: req.userID });
-  res.status(201).json({ message: 'Note created successfully' });
-};
-
-export default createNotesController;
--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,4 @@
 export { default as createUserService } from './create_user.service.js';
 export { default as getUserService } from './get_user.service.js';
-export { default as createNoteService } from './create_notes.service.js';
+export { default as createNoteService } from './create_note.service.js';
 export { default as deleteNoteService } from './delete_note.service.js';
