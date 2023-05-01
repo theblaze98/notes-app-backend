cccfe08ec7464d99eab352d7227571ceae7379ca
Shammael <shammamnd2015@gmail.com>
2023-05-01 09:51:21 -0400

    refactor: :truck: add v1 support to index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 // import './config/database.js';
-import routes from './routes/index.js';
+import v1Routes from './routes/v1/index.js';
 import errorRouteHandler from './routes/error_handler.route.js';
 
 const app = express();
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(morgan('dev'));
 
-app.use('api/v1', routes);
+app.use('api/v1', v1Routes);
 app.use(errorRouteHandler);
 
 app.listen(PORT, () => {
