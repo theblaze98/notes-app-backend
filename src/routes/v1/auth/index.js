7c429f5741a630888be6c607c6936f50a5698a71
Shammael <shammamnd2015@gmail.com>
2023-05-01 09:49:29 -0400

    refactor: :recycle: add v1 route support

--- src/routes/v1/auth/index.js
+++ src/routes/v1/auth/index.js
@@ -1,13 +1,13 @@
 import {
   loginController,
   signupController
-} from '../../controllers/auth/index.js';
+} from '../../../controllers/auth/index.js';
 import {
   loginMiddleware,
   signupMiddleware
-} from '../../middlewares/auth/index.js';
+} from '../../../middlewares/auth/index.js';
 import { Router } from 'express';
-import controllerErrorBoundaries from '../../controllers/controller_error_boundaries_handler.js';
+import controllerErrorBoundaries from '../../../controllers/controller_error_boundaries_handler.js';
 
 const router = Router();
 
--- src/routes/v1/notes/index.js
+++ src/routes/v1/notes/index.js
@@ -4,11 +4,11 @@ import {
   deleteNoteController,
   getNoteController,
   getNotesController
-} from '../../controllers/notes/index.js';
-import createNoteMiddleware from '../../middlewares/note/create_note.middleware.js';
-import deleteNoteMiddleware from '../../middlewares/note/delete_note.middleware.js';
-import { isAuthenticatedMiddleware } from '../../middlewares/user/index.js';
-import controllerErrorBoundaries from '../../controllers/controller_error_boundaries_handler.js';
+} from '../../../controllers/notes/index.js';
+import createNoteMiddleware from '../../../middlewares/note/create_note.middleware.js';
+import deleteNoteMiddleware from '../../../middlewares/note/delete_note.middleware.js';
+import { isAuthenticatedMiddleware } from '../../../middlewares/user/index.js';
+import controllerErrorBoundaries from '../../../controllers/controller_error_boundaries_handler.js';
 
 const router = Router();
 
