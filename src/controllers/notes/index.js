d6efab93e0f87a1124920a42bae3aa7374ef1b19
Shammael <shammamnd2015@gmail.com>
2023-04-30 22:21:04 -0400

    fix: :bug: fix file extension error

--- src/controllers/notes/index.js
+++ src/controllers/notes/index.js
@@ -1,3 +1,3 @@
 export { default as createNoteController } from './create_note.controller.js';
 export { default as deleteNoteController } from './delete_note.controller.js';
-export { default as getNotesController } from './get_notes.controller.JS';
+export { default as getNotesController } from './get_notes.controller.js';
