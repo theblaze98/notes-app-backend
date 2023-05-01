4b4e55ea3125eea2dbfdd377a3a4bfe5dcca672b
Shammael <shammamnd2015@gmail.com>
2023-04-30 22:57:58 -0400

    feat: :sparkles: add errorRouteHandler and script to genrate jwt

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 // import './config/database.js';
 import routes from './routes/index.js';
+import errorRouteHandler from './routes/error_handler.route.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,7 +14,11 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.use('api/v1', routes);
+app.use(errorRouteHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listen in http://localhost:${PORT}`);
 });
+
+// Generate jwt
+// require('crypto').randomBytes(64).toString('hex')
