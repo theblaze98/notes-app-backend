3f047be10b9e435d6d30ee9f8ea9e2387170a949
Shammael <shammamnd2015@gmail.com>
2023-05-04 10:37:57 -0400

    feat: :sparkles: add populate support for user in getNotesService

--- src/services/get_notes.service.js
+++ src/services/get_notes.service.js
@@ -3,7 +3,12 @@ import NoteModel from '../models/Note.js';
 const getNotesService = async (pageNumber) => {
   return await NoteModel.paginate(
     { active: true },
-    { page: pageNumber, lean: true, limit: 10 }
+    {
+      page: pageNumber,
+      lean: true,
+      limit: 10,
+      populate: { path: 'createdBy', select: 'username email _id' }
+    }
   );
 };
 
