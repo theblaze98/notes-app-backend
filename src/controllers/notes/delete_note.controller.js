4240f612b2b1111be24b22bf5c8bec627dcc02d3
Shammael <shammamnd2015@gmail.com>
2023-05-04 10:10:26 -0400

    refactor: :recycle: return note data instead of message

--- src/controllers/notes/delete_note.controller.js
+++ src/controllers/notes/delete_note.controller.js
@@ -2,8 +2,14 @@ import { deleteNoteService } from '../../services/index.js';
 
 const deleteNoteController = async (req, res) => {
   const { id } = req.query;
-  await deleteNoteService(id);
-  return res.status(200).json({ message: 'Note deleted successfully' });
+  const noteDB = await deleteNoteService(id);
+  return res
+    .status(200)
+    .json({
+      description: noteDB.description,
+      id: noteDB._id,
+      title: noteDB.title
+    });
 };
 
 export default deleteNoteController;
