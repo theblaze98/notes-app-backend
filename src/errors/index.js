24dce24815247e509c8430790bad6e22552e49c0
Shammael <shammamnd2015@gmail.com>
2023-05-06 08:22:02 -0400

    feat: :sparkles: Add JoiError class

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,2 +1,3 @@
 export { default as BadRequestError } from './bad_request.error.js';
 export { default as UnauthorizedError } from './unauthorized.error.js';
+export { default as JoiError } from './joi.error.js';
