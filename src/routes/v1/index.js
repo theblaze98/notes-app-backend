96b43460139877d6efdc8b352878598cc774c48e
Shammael <shammamnd2015@gmail.com>
2023-05-06 08:25:39 -0400

    fix: :bug: remove the new before Router()

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 import authRoutes from './auth/index.js';
 import userRoutes from './user/index.js';
 import noteRoutes from './notes/index.js';
-
 dotenv.config();
-const router = new Router();
+
+const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
