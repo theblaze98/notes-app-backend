00725edbd7f56f2a603377352edb3fbc209332fa
Shammael <shammamnd2015@gmail.com>
2023-05-06 08:22:51 -0400

    feat: :sparkles: call connect function from index

--- src/index.js
+++ src/index.js
@@ -5,20 +5,23 @@ import morgan from 'morgan';
 import v1Routes from './routes/v1/index.js';
 import errorRouteHandler from './routes/error_handler.route.js';
 import v1SwaggerDoc from './routes/v1/swagger.js';
+import connect from './config/database.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
 
-app.use('api/v1', v1Routes);
+app.use('/api/v1', v1Routes);
+
 app.use(errorRouteHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   v1SwaggerDoc(app, PORT);
+  await connect();
   console.log(`Server listen in http://localhost:${PORT}`);
 });
 
